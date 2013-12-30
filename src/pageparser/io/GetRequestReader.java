f4897285dc54310b473cb313f8781b47b521ba63
Nano <matzlarsson94@gmail.com>
2013-12-30 05:09:20 +0100

    Updated and fixed IO-classes

--- src/pageparser/io/GetRequestReader.java
+++ src/pageparser/io/GetRequestReader.java
@@ -8,12 +8,12 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.Map;
 
-public class PostRequestReader implements Reader {
+public class GetRequestReader implements Reader {
 	
 	private String url;
 	private Map<String, String> data, headers;
 
-	public PostRequestReader(String url, Map<String, String> data, Map<String, String> requestHeaders){
+	public GetRequestReader(String url, Map<String, String> data, Map<String, String> requestHeaders){
 		this.url = url;
 		this.data = data;
 		this.headers = requestHeaders;
@@ -21,20 +21,21 @@ public class PostRequestReader implements Reader {
 	
 	@Override
 	public String getPageSource() throws IOException {
-		URL obj = new URL(url);
-		HttpURLConnection con = (HttpURLConnection)obj.openConnection();
- 
-		//add request header
-		con.setRequestMethod("POST");
-		for(String key : headers.keySet()){
-			con.setRequestProperty(key, headers.get(key));
-		}
- 
 		//setup parameters
 		String urlParameters = "";
 		for(String key : data.keySet()){
 			urlParameters += (urlParameters.length()>0?"&":"")+key+"="+data.get(key);
 		}
+		
+		URL obj = new URL(url+(urlParameters.length()>0?"?"+urlParameters:""));
+		HttpURLConnection con = (HttpURLConnection)obj.openConnection();
+ 
+		//add request header
+		con.setRequestMethod("GET");
+		for(String key : headers.keySet()){
+			con.setRequestProperty(key, headers.get(key));
+		}
+ 
  
 		// Send post request
 		con.setInstanceFollowRedirects(true);
@@ -46,8 +47,8 @@ public class PostRequestReader implements Reader {
 		wr.close();
  
 		int responseCode = con.getResponseCode();
-		System.out.println("\nSending 'POST' request to URL : " + url);
-		System.out.println("Post parameters : " + urlParameters);
+		System.out.println("\nSending 'GET' request to URL : " + url);
+		System.out.println("Get parameters : " + urlParameters);
 		System.out.println("Response Code : " + responseCode);
  
 		BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
